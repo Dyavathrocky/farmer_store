fc26bfce71e0d8d37d0b87164bad7a4f1b6a67ac
rakesh dyavath <davathrak@gmail.com>
2022-11-22 17:30:19 +0530

    added users

--- farmer_proj/urls.py
+++ farmer_proj/urls.py
@@ -19,4 +19,6 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls'), name='pages'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup", include("accounts.urls"), name="signup"),
 ]
